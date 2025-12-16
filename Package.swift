b440c926ca726b2a9a01337eafaf9d6de5ddc768
Brian Guertin <brian@joingo.com>
2025-12-15 19:26:44 -0500

    dynamic lib

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,10 +6,12 @@ import PackageDescription
 let package = Package(
     name: "SwiftyBeaver",
     products: [
-        .library(name: "SwiftyBeaver", targets: ["SwiftyBeaver"])
+        .library(name: "SwiftyBeaver", type: .dynamic, targets: ["SwiftyBeaver"])
     ],
     targets: [
-        .target(name: "SwiftyBeaver", path: "Sources"),
+        .target(name: "SwiftyBeaver", path: "Sources", swiftSettings: [
+                                            .unsafeFlags(["-enable-library-evolution"])
+                                        ]),
         .testTarget(name: "SwiftyBeaverTests", dependencies: ["SwiftyBeaver"]),
     ],
     swiftLanguageVersions: [.v5]
